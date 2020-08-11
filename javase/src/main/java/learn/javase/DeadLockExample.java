41bda3c565176f38d7253405e3389a9641a692fc
WangShiDong <wang_sd@bayss.com>
2020-08-11 18:08:22 +0800

    增加多线程相关示例

---
+++ javase/src/main/java/learn/javase/DeadLockExample.java
@@ -0,0 +1,55 @@
+package learn.javase;
+
+/**
+ * 手写一个死锁的示例
+ *
+ * @author Zephyr
+ * @date 2020/8/11.
+ */
+public class DeadLockExample {
+
+    //必须有两个可以被加锁的对象才能产生死锁，只有一个不会产生死锁问题
+    private final Object lock1 = new Object();
+    private final Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        new DeadLockExample().testDeadLock();
+    }
+
+
+    /**
+     * 为了便于让两个线程分别锁住其中一个对象，
+     * 一个线程锁住 lock1，然后一直等待 lock2，
+     * 另一个线程锁住 lock2，然后一直等待 lock1，
+     * 两个线程互相持有对方需要的锁，死锁产生
+     */
+    void testDeadLock() {
+        Thread t1 = new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println(Thread.currentThread().getName() + "获取到锁 lock1");
+                SleepUtil.sleep(100);
+                synchronized (lock2) {
+                    System.out.println(Thread.currentThread().getName() + "获取到锁 lock2");
+                    SleepUtil.sleep(100);
+                }
+            }
+            System.out.println(Thread.currentThread().getName() + " finished");
+        }, "thread1");
+        Thread t2 = new Thread(() -> {
+            synchronized (lock2) {
+                System.out.println(Thread.currentThread().getName() + "获取到锁 lock2");
+                SleepUtil.sleep(100);
+                synchronized (lock1) {
+                    System.out.println(Thread.currentThread().getName() + "获取到锁 lock1");
+                    SleepUtil.sleep(100);
+                }
+            }
+            System.out.println(Thread.currentThread().getName() + " finished");
+        }, "thread2");
+
+
+        t1.start();
+        t2.start();
+    }
+
+}
