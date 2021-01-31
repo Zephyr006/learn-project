9b8eae14a30248aee99d7f08dce0f059ad8d0400
Zephyr <wangxiansen512@163.com>
2021-01-31 22:42:40 +0800

    JsonUtil.java

--- base/src/main/java/learn/base/utils/JsonUtil.java
+++ base/src/main/java/learn/base/utils/JsonUtil.java
@@ -1,16 +1,21 @@
 package learn.base.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.type.MapType;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,14 +29,18 @@ import java.util.Map;
  */
 public class JsonUtil {
     private static final Logger logger = LoggerFactory.getLogger(JsonUtil.class);
-    private static ObjectMapper MAPPER = buildObjectMapper();
+    private static ObjectMapper MAPPER = new ObjectMapper();
 
-    private static ObjectMapper buildObjectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-
-        mapper.registerModule(new JavaTimeModule());
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        return mapper;
+    static {
+        // JDK8新时间类支持： 序列化时带有T的问题，自定义格式化字符串
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        MAPPER.registerModule(javaTimeModule);
+        // Date (and date/time) and Date-based things like {@link java.util.Calendar}s are to be serialized as numeric timestamps
+        MAPPER.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        // 如果JSON字符串中包含Java类中没有的字段，不再抛出异常 (默认导致UnrecognizedPropertyException异常)
+        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
 
@@ -48,7 +57,7 @@ public class JsonUtil {
         try {
             return MAPPER.readValue(text, clazz);
         } catch (JsonProcessingException e) {
-            logger.error("JsonUtil.parseObject error. ", e);
+            logger.error("JsonUtil.parseObject error, original text = " + text, e);
         }
         return null;
     }
@@ -67,7 +76,7 @@ public class JsonUtil {
             ObjectReader reader = MAPPER.readerFor(clazz);
             return reader.<T>readValues(text).readAll();
         } catch (IOException e) {
-            logger.error("JsonUtil.parseArray error. ", e);
+            logger.error("JsonUtil.parseArray error, original text = " + text, e);
         }
         return null;
     }
